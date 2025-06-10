48a5a830692dee44f9e89984fefc4954b2c768bf
xavi <antunez.xavi.grao@gmail.com>
2025-06-10 09:59:31 +0200

    super commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ const auth = async (req, res, next) => {
   const token = req.headers.authorization;
   // Si no hay token => 401
   if (!token) {
-    res.status(401).send("Missing auth header");
+    res.status(401).send({msg: "Missing auth header"});
     return;
   }
   // Verificar token con jwt secret
@@ -15,14 +15,14 @@ const auth = async (req, res, next) => {
   try {
     payload = jsonwebtoken.verify(token, JWT_SECRET);
   } catch (error) {
-    res.status(401).send("Invalid auth header");
+    res.status(401).send({msg: "Invalid auth header"});
   }
   // Obtener el userId del payload
   const userId = payload.userId;
   // Prevenir edge case: Find de ese user by userId
   const user = await User.findById(userId);
   if (!user) {
-    res.status(401).send("Invalid user"); // EDGE CASE
+    res.status(401).send({msg: "Invalid user"}); // EDGE CASE
     return;
   }
   // Guardo user en la request
