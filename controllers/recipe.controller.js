8b88c98993fc45b7595ad46dc579dbbbf6527e86
xavi <antunez.xavi.grao@gmail.com>
2025-07-01 10:58:54 +0200

    Endpoint structure

---
+++ controllers/recipe.controller.js
@@ -0,0 +1,33 @@
+const { Ingredient } = require("../models/ingredient.model");
+
+const getRecipes = async (req, res) => {
+  // #swagger.tags = ['Recipes']
+  // #swagger.summary = 'To get AI generated recipes'
+  try {
+    let { ingredients } = req.query;
+    if (!ingredients) {
+      res.status(404).send({ msg: "No ingredients passed" });
+    }
+    if (!Array.isArray(ingredients)) {
+      ingredients = [ingredients];
+    }
+    // ingredients is an array of ids
+    const ingredientDocs = await Ingredient.find({ _id: { $in: ingredients } });
+    const ingredientNames = ingredientDocs.map((ing) => ing.name);
+    const generatedRecipes = [
+      {
+        id: "2",
+        title: "Paella",
+        description: "Arroz con cosas",
+        ingredients: JSON.stringify(ingredientNames),
+      },
+    ];
+    res.send({ recipes: generatedRecipes });
+  } catch (error) {
+    // TODO: Improve error handling
+    console.log(error);
+    res.status(404).send({ error: error.name, message: error._message });
+  }
+};
+
+module.exports = { getRecipes };
