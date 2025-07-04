6aac97afc1e964ab7328cdd7c3a4a82ab5adbf40
Ruben-Garrido-Piquer <rubengarrido241@gmail.com>
2025-07-04 13:46:07 +0200

    Change recipe controlles and routes

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { getRecipes, getUserRecipes, createRecipe } = require("../controllers/recipe.controller");
 const router = express.Router();
 
-router.get("/user", getUserRecipes);
+const { auth: authenticate } = require("../middlewares/auth");
+
+const {
+    getRecipes,
+    createRecipe,
+    getUserRecipes,
+} = require("../controllers/recipe.controller");
+
 router.get("/", getRecipes);
-router.post("/", createRecipe);
+router.get("/user", authenticate, getUserRecipes);
+router.post("/", authenticate, createRecipe);
 
 module.exports = router;
